a138d9de985900704bdf804db2187c4a899765de
Wong <thatlostboy@gmail.com>
2018-11-10 00:04:57 -0800

    Added working mongo post

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,15 +1,18 @@
 const axios = require("axios");
 const router = require("express").Router();
+const db = require("../models")
 
 
 router.get("/books", (req, res) => {
-   let urlReq = "https://www.googleapis.com/books/v1/volumes?q=harry+potter&key=" + process.env.GOOGLE_BOOKS_APIKEY
-  console.log("Testing.  a whole bunch of lines lines lines. .")
-  console.log(urlReq)
+  const search = req.query.search
+  const params = { q: search , key: process.env.GOOGLE_BOOKS_APIKEY}
+  const urlReq = "https://www.googleapis.com/books/v1/volumes"
+  // console.log("Testing.  a whole bunch of lines lines lines. .")
+  // console.log(urlReq)
   axios
-    .get(urlReq)
+    .get(urlReq, { params })
     .then(({data: { items }})=> {
-      console.log(items)
+      // console.log(items)
       let newList = items.map(book => {
         const { title , authors, description, imageLinks, infoLink } = book['volumeInfo']
         const { thumbnail } = imageLinks
@@ -17,12 +20,14 @@ router.get("/books", (req, res) => {
       })
       return res.json(newList)
     })
-
-  
+    .catch(err => res.status(422).json(err))
 });
 
 router.post("/books", (req, res) => {
   console.log("API just got called with body: ", req.body)
+  db.Books.create(req.body)
+    .then(result => {console.log(result)})
+  
 });
 
 router.get("/books/:id", (req, res) => {
--- server.js
+++ server.js
@@ -1,20 +1,24 @@
 require('dotenv').config()
-console.log(process.env)
 
+const mongoose = require("mongoose")
 const express = require("express");
-const path = require("path");
-
-const PORT = process.env.PORT || 3001;
-const app = express();
 const apiRoutes = require("./routes/apiRoutes")
+const path = require("path");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
 
 // Define API routes here
 app.use("/api", apiRoutes);
